ddc0a6c094d4c346ed9b9c665e91dcc727c5b42b
sudhanshu shekhar <contact.sidshekhar@gmail.com>
2024-03-07 10:40:00 +0530

    Update types.js

--- backend/types.js
+++ backend/types.js
@@ -2,7 +2,8 @@ const zod = require("zod");
 
 const createTodo = zod.object({
     title: zod.string(),
-    description: zod.string()
+    description: zod.string(),
+    completed:zod.boolean()
 })
 
 const updateTodo = zod.object({

dab4c6d36a61bb0c00664bb6d6f93ec13cd11891
sudhanshu shekhar <contact.sidshekhar@gmail.com>
2024-03-07 10:39:02 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,9 @@ mongoose.connect("mongodb+srv://sid:***@cluster0.i1unc7g.mongodb.net/Todo")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
-    completed: Boolean
+    completed: {
+        type:Boolean,
+        default:false
 })
 
 const todo = mongoose.model('todos', todoSchema);
